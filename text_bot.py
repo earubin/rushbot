ec848c547f78789b0896a414dca76764eea4f594
earubin <43094785+earubin@users.noreply.github.com>
2020-12-29 16:59:45 -0600

    Update text_bot.py

--- text_bot.py
+++ text_bot.py
@@ -49,7 +49,7 @@ class TextBot():
                         to = self.to_number)
                 if len(self.args) > 0:
                     for i in self.args:
-                        self.message = client.messages                         .create(
+                        self.message = client.messages.create(
                             body = message,
                             from_ = self.from_number,
                             to = i)
@@ -69,60 +69,4 @@ class TextBot():
     def get_from(self):
         return self.from_number
 
-
-# In[5]:
-
-
-'''
-
-Put whatever names you want to text in here. I will organize this a little better at some point, but for now, it's just two arrays zipped into a dictionary.
-
-
-names = ['Jack Aiello', 'Ben Goddard', 'Daniel Glassman', 'Brian Carter', 
-         'Jimmy Ewers', 'Ryan Way', 'Daniel Gankov', 'Jack Gillespie', 
-         'Ronan Kenkare', 'Wyatt Flynn', 'Jack Banks', 'Elliot Rubin']
-
-numbers = ['+12244564568', '+16193940968', '+18477511005', '+16302546002',
-           '+17083051183', '+18155923507', '+17736033201', '+18474527048',
-           '+15857058332', '+17082651576', '+17735755623', '+18479242986']
-
-nums_dict = dict(zip(names, numbers))
-#bot = TextBot(account_sid, auth_token, '+12705944959', '+18479242986', [i for i in nums_dict.values() if i != nums_dict['Elliot Rubin']])
-
-'''
-
-
-# In[7]:
-
-
-bot = TextBot(account_sid, auth_token, '+12705944959', '+12244564568')
-bot.send_message('Good morning, king. Make sure you complete your task for today.' + '\n' + '\n' +
-                 'This message is automated and I cannot yet see your replies.')
-
-
-# In[17]:
-
-
-a = datetime.datetime.today()
-numdays = 10
-date_list = []
-for x in range (0, numdays):
-    date_list.append(a + datetime.timedelta(days = x))
-date_list
-
-
-# In[11]:
-
-
-new_bot = TextBot(account_sid, auth_token, '+12705944959', nums_dict['Jack Aiello'])
-new_bot.send_message('Can you massage my back?')
-
-
-# In[ ]:
-
-
-#while datetime.datetime.today() < date_list[len(date_list) - 1]:
-    #if datetime.datetime.today().time() == datetime.time(9,0,0) or datetime.datetime.today().time() == datetime.time(15,0,0) or datetime.datetime.today().time() == datetime.time(18,0,0):
-        #new_bot.send_message('Good morning, king. Make sure you complete your task for today.' , '\n', '\n' ,
-                             #'This message is automated and I cannot yet see your replies.')
 
